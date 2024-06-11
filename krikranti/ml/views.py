b340590584881a34d81b9787ace0e6f983ab8e4f
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2024-06-11 16:47:25 +0545

    jun 11

--- krikranti/ml/views.py
+++ krikranti/ml/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-# import torch
-# from PIL import Image
-# import torchvision.transforms as transforms
+ #import torch
+from PIL import Image
+ #import torchvision.transforms as transforms
 
 # Load model
 # model = torch.load('/')

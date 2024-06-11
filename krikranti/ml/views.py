1bb32ad0b17cd1a2939b2e140a8a9522c7e6fd8f
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2024-06-11 15:16:23 +0545

    jun 11

--- krikranti/ml/views.py
+++ krikranti/ml/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+# import torch
+# from PIL import Image
+# import torchvision.transforms as transforms
+
+# Load model
+# model = torch.load('/')
 
 
 def welcome(request):
-
-    return HttpResponse("Hello world")
+    return render(request, 'index.html')
 
 # Create your views here.

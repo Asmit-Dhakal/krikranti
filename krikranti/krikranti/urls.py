7d9bb9add0a67470636d0eef9766726b57813984
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2024-06-12 16:51:35 +0545

    jun 12

--- krikranti/krikranti/urls.py
+++ krikranti/krikranti/urls.py
@@ -18,14 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as authentication_views
 
-
 urlpatterns = [
     path('', include('ml.urls')),
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
-    path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'),
-         name='login'),
-    path('logout/', authentication_views.LogoutView.as_view(template_name='shop/index.html'),
-         name='logout'),
-    path('profile/', user_views.profilepage, name='profile'),
+    # path('register/', user_views.register, name='register'),
+    # path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'),
+    #      name='login'),
+    # path('logout/', authentication_views.LogoutView.as_view(template_name='shop/index.html'),
+    #     name='logout'),
+    # path('profile/', user_views.profilepage, name='profile'),
 ]
